b83fbd367bd2e3e7546fa1500381aeeb645ba8f3
Raphael Feitosa <raphaelcs2@gmail.com>
2021-05-05 21:25:29 -0300

    Pagination

---
+++ backend/src/main/java/com/raphaelfeitosa/vendas/service/SaleService.java
@@ -0,0 +1,29 @@
+package com.raphaelfeitosa.vendas.service;
+
+import com.raphaelfeitosa.vendas.dto.SaleDTO;
+import com.raphaelfeitosa.vendas.entities.Sale;
+import com.raphaelfeitosa.vendas.repositories.SaleRepository;
+import com.raphaelfeitosa.vendas.repositories.SellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class SaleService {
+
+    @Autowired
+    private SaleRepository repository;
+
+    @Autowired
+    private SellerRepository sellerRepository;
+
+    @Transactional(readOnly = true)
+    public Page<SaleDTO> findAll(Pageable pageable) {
+        sellerRepository.findAll();
+        Page<Sale> result = repository.findAll(pageable);
+
+        return result.map(x -> new SaleDTO(x));
+    }
+}
